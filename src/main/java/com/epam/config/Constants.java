f93e610530d8c1a1854571939a685b47bd358c79
Dzmitry Danilau <Dzmitry_Danilau@epam.com>
2019-05-21 17:30:45 +0300

    Add drop down list to data engine file

--- src/main/java/com/epam/config/Constants.java
+++ src/main/java/com/epam/config/Constants.java
@@ -6,13 +6,13 @@ public class Constants {
     public static final String FILE_TEST_DATA = "DataEngine.xlsx";
     public static final String FILE_OR = "OR.properties";
 
-    public static final int TEST_STEPS_COUNT = 10;
     public static final int COL_TEST_CASE_ID = 0;
     public static final int COL_TEST_SCENARIO_ID = 1;
-    public static final int COL_PAGE_OBJECT = 3;
-    public static final int COL_ACTION_KEYWORD = 4;
-    public static final int COL_DATA_SET = 5;
-    public static final int COL_TEST_STEP_RESULT = 6;
+    public static final int COL_PAGE_NAME = 3;
+    public static final int COL_PAGE_OBJECT = 4;
+    public static final int COL_ACTION_KEYWORD = 5;
+    public static final int COL_DATA_SET = 6;
+    public static final int COL_TEST_STEP_RESULT = 7;
 
     public static final int COL_RUN_MODE = 2;
     public static final int COL_RESULT = 3;
